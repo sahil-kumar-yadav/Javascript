1ca7672ce2612b1c195138b260743e15384fa68a
yadavsk098 <yadavsk098@gmail.com>
2024-07-05 20:17:27 +0530

    Project 3 Styles added

--- Part 2/Project 3 Digital Clock/script.js
+++ Part 2/Project 3 Digital Clock/script.js
@@ -10,3 +10,28 @@ setInterval(function () {
     date.innerHTML = dateobj.toLocaleDateString()
 }, 1000)
 
+
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours() % 12; // Convert to 12-hour format
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hourHand = document.querySelector('.hour');
+    const minHand = document.querySelector('.min');
+    const secondHand = document.querySelector('.second');
+
+    const hourDeg = (360 / 12) * hours - 180 ;
+    const minDeg = (360 / 60) * minutes  - 180 ;
+    const secondDeg = (360 / 60) * seconds - 180 ;
+
+    minHand.style.transform = `rotate(${minDeg}deg)`;
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    
+
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+updateClock(); // Initial update
