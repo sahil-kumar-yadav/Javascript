48af5286a245222156c882b48fa6ce8ea43acc62
yadavsk098 <yadavsk098@gmail.com>
2024-07-12 21:56:37 +0530

    Project 5 completed

---
+++ Part 2/Project 5 Keyboard keypress/script.js
@@ -0,0 +1,21 @@
+const insert = document.getElementById("insert")
+
+
+window.addEventListener("keydown", function (e) {
+    insert.innerHTML = `
+    <table>
+  <tr>
+    <th>Key</th>
+    <th>Key Code</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>${e.key === " " ? "Space":e.key}</td>
+    <td> ${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+</table>
+    
+    `
+})
